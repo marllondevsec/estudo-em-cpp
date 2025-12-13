f68ef39102d6c5059e0f69e3cb119b0fa53a8c50
Root <ghostkernel@Kali>
2025-12-12 21:41:56 -0300

    variaveis

---
+++ ano.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int idade=0;
+	int ano_atual=0;
+	int ano_de_nascimento=0;
+
+	cout << "qual o ano atual?:\n";
+	cin >>  ano_atual;
+
+	cout << "qual a sua idade?:\n";
+	cin >> idade;
+
+
+	ano_de_nascimento = idade - ano_atual;
+
+
+	cout << "voce nasceu em" << ano_de_nascimento;
+
+
+
+return 0;
+
+}
---
+++ variaveis.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int vidas=0;
+char letra='b';
+double decimal=5.2;
+float decimal2=5.2;
+bool vivo=true;
+string nome="marllon";
+
+
+cout << "digite o numero de vidas";
+cin >> vidas;
+
+cout << vidas;
+
+cin.get();
+
+return 0;
+
+}
