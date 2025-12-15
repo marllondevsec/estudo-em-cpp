fa0b346c7a91f2e6c075105096b7412af77daf30
Marllon <marllondevsec.comercial@gmail.com>
2025-12-15 00:28:36 -0300

    codigos pra inversao de variavel e suas tipificaçoes.

--- IncrementoV.cpp
+++ IncrementoV.cpp
@@ -14,9 +14,14 @@ int main(){
 
 		n1+= 5; //usando (operador)= como -=,+=, /=, e uma forma mais eficiente pra adicionar ou subtrair valores a variaveis.
 
-		std:: cout << n1 << std::endl;
+		std:: cout << ++n1 << std::endl; // ao utilizar o ++ no cout deve ser feito antes da variavel, pois assim ele e adicionado antes, se nao ele ira printar o numero base e depois ira adicionar o valor.
 
 
+
+		//EXEMPLO: n1=n1+10; = n1+=10;
+
+
+		
 
 
 return 0;
---
+++ InverterV.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+	int main(){
+
+
+
+		int num=10;
+
+			std::cout << num <<  std::endl;
+
+			num = num*-1; //assim voce inverte a variavel gravando na memoria.
+
+			std::cout << -num  << std::endl; //isso nao grava o valor na variavel, mas mostra o valor invertido.
+
+			std::cout << num  << std::endl; 
+
+
+		int abc=20;
+
+			abc=-abc;//melhor forma pra inverter o valor das variaveis
+
+
+			std::cout << abc;
+
+
+
+return 0; 
+
+}
