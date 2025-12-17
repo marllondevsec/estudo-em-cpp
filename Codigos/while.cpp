a2b0080e1cb48981042a1e9fb635c37b66f2550e
Marllon <marllondevsec.comercial@gmail.com>
2025-12-17 03:15:05 -0300

    testes em looping com while

---
+++ Codigos/while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+	int n=0,n1=0;
+		while(n<1000000)
+		{ std::cout << n << std::endl; std::cout << "teste de looping\n";
+		 n+=10;}
+
+
+	while(n1++<10000){ std::cout << "contando -" << n1 << std::endl; if(n1 == 300){break;}}
+return 0;}
