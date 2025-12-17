cdd88937994ae3c1fb48641f1a3a6b224c436189
Marllon <marllondevsec.comercial@gmail.com>
2025-12-17 20:11:31 -0300

    testes com o comando do while

---
+++ Codigos/DoWhile.cpp
@@ -0,0 +1,8 @@
+// assim o comando e executado pelo menos uma vez.
+#include <iostream>
+
+int main (){
+	int n1=0;
+	do{ std::cout << "contando - " << n1 << std::endl;}while( n1++ < 100);
+
+return 0; }
